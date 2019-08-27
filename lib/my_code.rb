dd0f88608a05aadad1597aaa9da05ba58123f6f1
mohamad farahmandifard <ticticb80om@gmail.com>
2019-08-27 02:56:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # Your Code Here
+
+def my_own_map(source_array)
+  answer.push(source_array*-1)
+end
+answer = 
+my_own_map(x) do
+  answer = []
+  source_array.length.times
+    answer.push(source_array.map * -1)
+    
